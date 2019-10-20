e79590381ad30acab5d70b7c222315fd5b1ecc75
Tevfik <tevfiktayfun3@gmail.com>
2019-10-20 18:58:38 +0300

    first update

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public float spawnRate = 1f;
+    public GameObject Hexagon;
+    private float nextTimetoSpawn = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time >= nextTimetoSpawn)
+        {
+            Instantiate(Hexagon, Vector3.zero, Quaternion.identity);
+            nextTimetoSpawn = Time.time + 1f / spawnRate;
+        }
+    }
+}
